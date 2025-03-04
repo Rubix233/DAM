8c9f0adde899cb2da2f410e15d216e8ec0cd6c9d
Rubix233 <andyjan.20@campuscamara.es>
2025-03-04 22:24:42 +0100

    Act 1-5

---
+++ Prog/AEF Cadenas/src/Act5Cad.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andy Jan
+ */
+public class Act5Cad {
+
+    public static String monoAlf(int clave, String intro) {
+        String codificado = "";
+        int length = intro.length() - 1;
+
+        //Recorer todos los caracteres
+        for (int i = 0; i <= length; i++) {
+            //Establecer codigo ASCII y el objetivo
+            int charInt = (int) intro.charAt(i);
+            int target = charInt + clave%26;//Por si se cuelan
+            
+            //Si es letra
+            if (charInt >= 65 && charInt <= 90 
+                    || charInt >= 97 && charInt <= 122) {
+                
+                //Si es minuscula y se sale de rango
+                if (charInt >= 97) {
+                    if (target < 97) {
+                        target = 123 - (97 - target);
+                    }//Fin Si
+                    if (target > 122) {
+                        target = (target - 122) + 96;
+                    }//Fin Si
+                } else { //Si es mayus y se sale de rango
+                    if (target < 65) {
+                        target = 91 - (65 - target);
+                    }//Fin Si
+                    if (target > 90) {
+                        target = (target - 90) + 64;
+                    }//Fin Si
+                }//Fin Si
+                codificado += (char) target;
+            } else {//Si no es letra se deja igual
+                codificado += intro.charAt(i);
+            }//Fin Si
+        }
+        return codificado;
+    }
+    public static String monoAlfDecode(int clave, String intro){
+        return monoAlf(clave*-1,intro);
+    }
+    
+    public static void main(String[] args) {
+        //Entorno
+        String introducido;
+        int clave;
+        
+        //Algoritmo
+        System.out.print("Introduzca una cadena: ");
+        introducido = Leer.dato();
+        System.out.print("Introduzca clave para codificar: ");
+        clave = Leer.datoInt();
+        System.out.println("Su mensaje codificado:");
+        System.out.println(monoAlf(clave,introducido));
+        System.out.println("Decodificado:");
+        System.out.println(monoAlfDecode(clave,monoAlf(clave,introducido)));
+
+    }//Fin Programa
+}
