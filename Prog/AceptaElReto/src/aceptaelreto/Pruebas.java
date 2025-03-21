e2875a928d797598fb264b489c5f8e2333a402d3
Rubix233 <andyjan.20@campuscamara.es>
2025-03-21 16:35:46 +0100

    Diagramas

---
+++ Prog/AceptaElReto/src/aceptaelreto/Pruebas.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aceptaelreto;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Pruebas {
+    public static void main(String[] args){
+        int a = Integer.parseInt(("2 ").trim());
+        
+        System.out.println(a);
+    }
+}
