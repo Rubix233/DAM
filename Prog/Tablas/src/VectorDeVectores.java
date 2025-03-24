ae65d3cbfe5a3853441cff8e32d2f0869e1fff20
Rubix233 <andyjan.20@campuscamara.es>
2025-03-24 14:54:54 +0100

    tabla

---
+++ Prog/Tablas/src/VectorDeVectores.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andy Jan
+ */
+public class VectorDeVectores {
+    public static void main(String[] args){
+        int[][] tabla = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+
+        
+        System.out.println(tabla[0][1]);
+        
+    }
+}
