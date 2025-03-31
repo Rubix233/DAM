14b1f11338bece7f7e6a61fa86aa19c2c473234a
Rubix233 <andyjan.20@campuscamara.es>
2025-03-31 14:49:50 +0200

    Format

--- Prog/Registros/src/IntercambioSort.java
+++ Prog/Registros/src/IntercambioSort.java
@@ -18,13 +18,13 @@ public class IntercambioSort {
 
         //Algoritmo
         limite = tabla.length - 1;
-        
+
         //Ascendente
-        for(i = 0; i<limite;i++){
+        for (i = 0; i < limite; i++) {
             pos = i;
             aux = tabla[i];
-            for(j = i+1;j<=limite;j++){
-                if(tabla[j] < aux){
+            for (j = i + 1; j <= limite; j++) {
+                if (tabla[j] < aux) {
                     aux = tabla[j];
                     pos = j;
                 }//Fin Si
@@ -32,15 +32,15 @@ public class IntercambioSort {
             tabla[pos] = tabla[i];
             tabla[i] = aux;
         }//Fin Para
-        
+
         System.out.println(Arrays.toString(tabla));
-        
+
         //Descendiente
-        for(i = 0; i<limite;i++){
+        for (i = 0; i < limite; i++) {
             pos = i;
             aux = tabla[i];
-            for(j = i+1;j<=limite;j++){
-                if(tabla[j] > aux){
+            for (j = i + 1; j <= limite; j++) {
+                if (tabla[j] > aux) {
                     aux = tabla[j];
                     pos = j;
                 }//Fin Si
