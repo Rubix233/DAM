2b9e5e3aacfd03a3b01478744cf74691c315f2c7
Rubix233 <andyjan.20@campuscamara.es>
2025-04-10 12:17:47 +0200

    Calendario

--- Prog/Calendario/src/Calendario.java
+++ Prog/Calendario/src/Calendario.java
@@ -1,5 +1,5 @@
 
-import java.util.Calendar;
+
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
@@ -34,28 +34,49 @@ public class Calendario {
     
     public static void main(String[] args){
         GregorianCalendar calendario;
-        int primerDia, anio;
-        byte mes;
-        
+        int anio;
+        byte mes,j,primerDia;
+
+        //Pedimos datos
         System.out.print("Introduce año: ");
         anio = Leer.datoInt();
         System.out.print("Introduce mes: ");
         mes = datoByte();
         mes--;
-        
+
         calendario = new GregorianCalendar(anio,mes,1);
         
-        if(calendario.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY){
+        //Establecemos el primer dia del mes
+        if(calendario.get(GregorianCalendar.DAY_OF_WEEK) == GregorianCalendar.SUNDAY){
             primerDia = 7;
         } else {
-            primerDia = calendario.get(Calendar.DAY_OF_WEEK) -1;
+            primerDia = (byte) (calendario.get(GregorianCalendar.DAY_OF_WEEK) -1);
         }
-
+        
+        System.out.println("Mon\tTue\tWed\tThu\tFri\tSat\tSun");
+        
+        //Rellenamos los dias vacios el principio del mes
+        for (byte i = 1 ; i < primerDia; i++){
+            System.out.print("\t");
+        }
+        
+        //Construimos el output
+        j=primerDia;
+        for (byte i = 1; i <= calendario.getActualMaximum(GregorianCalendar.DAY_OF_MONTH); i++) {
+            System.out.print(i);
+            if(j % 7 != 0){
+                System.out.print("\t");
+            } else {
+                System.out.print("\n");
+            }
+            j++;
+        }
+        /*
         System.out.println(calendario.getActualMaximum(Calendar.DAY_OF_MONTH));
         System.out.println(calendario.get(Calendar.DAY_OF_WEEK));
         System.out.println(primerDia);
 
-        
+        */
         //System.out.println(Locale.getDefault());
         
     }
