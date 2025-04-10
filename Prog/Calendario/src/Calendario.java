1c5bda8ead97a150d22e6666294a71bf4885947c
Rubix233 <andyjan.20@campuscamara.es>
2025-04-10 11:04:50 +0200

    a

--- Prog/Calendario/src/Calendario.java
+++ Prog/Calendario/src/Calendario.java
@@ -13,18 +13,43 @@ import java.util.Locale;
  * @author Andy Jan
  */
 public class Calendario {
+    public static byte datoByte() {
+        //Entorno
+        byte dato;
+        short numero;
+
+        //Algoritmo
+        numero = Leer.datoShort();
+
+        while (numero < 1 || numero > 12) {
+            System.out.print("Mes incorrecto, introduzca otro: ");
+            numero = Leer.datoShort();
+        }//Fin Mientras
+
+        dato = (byte) numero;
+        return dato;
+    }
+    
+    
+    
     public static void main(String[] args){
         GregorianCalendar calendario;
-        int primerDia;
+        int primerDia, anio;
+        byte mes;
         
-        calendario = new GregorianCalendar(2025,3,1);
+        System.out.print("Introduce año: ");
+        anio = Leer.datoInt();
+        System.out.print("Introduce mes: ");
+        mes = datoByte();
+        mes--;
+        
+        calendario = new GregorianCalendar(anio,mes,1);
         
         if(calendario.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY){
             primerDia = 7;
         } else {
             primerDia = calendario.get(Calendar.DAY_OF_WEEK) -1;
         }
-
 
         System.out.println(calendario.getActualMaximum(Calendar.DAY_OF_MONTH));
         System.out.println(calendario.get(Calendar.DAY_OF_WEEK));
