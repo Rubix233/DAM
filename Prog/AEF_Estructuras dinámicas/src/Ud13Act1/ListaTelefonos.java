46bc463b2e1326be038c31a813aff2fc687eb5a7
Rubix233 <andyjan.20@campuscamara.es>
2025-05-05 14:49:46 +0200

    Dinamico

---
+++ Prog/AEF_Estructuras dinámicas/src/Ud13Act1/ListaTelefonos.java
@@ -0,0 +1,97 @@
+package Ud13Act1;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andy Jan
+ */
+public class ListaTelefonos {
+    private Persona[] listaTelefonos;
+    private int nElementos;
+    
+    
+    public ListaTelefonos(){
+        listaTelefonos = new Persona[0];
+        nElementos = 0;
+    }
+    
+    //Cambiar el uso de variable aux por listaActual
+    private void unElementoMas(Persona[] listaActual){
+        //Entorno
+        this.nElementos++;
+        this.listaTelefonos = new Persona[this.nElementos];
+        
+        //Algoritmo
+        for(int i=0;i < listaActual.length; i++){
+            this.listaTelefonos[i] = listaActual[i];
+        }//Fin Para
+    }
+    private void unElementoMenos(Persona[] listaActual) {
+        if (listaActual.length > 0) {
+            this.nElementos--;
+            this.listaTelefonos = new Persona[this.nElementos];
+            int iViejo;
+            int iNuevo = 0;
+
+            for (iViejo = 0; iViejo < listaActual.length; iViejo++) {
+                if (listaActual[iViejo] != null) {
+                    this.listaTelefonos[iNuevo] = listaActual[iViejo];
+                    iNuevo++;
+                }//Fin Si
+            }//Fin Para
+        }//Fin Si
+
+
+    }
+    public void ponerValorEn(int pos, Persona p){
+        if(pos >= this.nElementos){
+            System.out.println("Indice fuera de rango");
+        } else {
+            this.listaTelefonos[pos] = p;
+        }//Fin Si
+    }
+    public Persona valorEn(int pos){
+        Persona buscada = null;
+        if(pos > this.nElementos){
+            System.out.println("Indice fuera de rango");
+        } else {
+            buscada = this.listaTelefonos[pos];
+        }//Fin Si
+        return buscada;
+    }
+    public int longitud(){
+        return this.nElementos;
+    }
+    public void aniadir(Persona p){
+        unElementoMas(this.listaTelefonos);
+        this.listaTelefonos[this.nElementos-1] = p;
+    }
+    public boolean eliminar(long tel){
+        boolean eliminado = false;
+        for (int i = 0; i < this.nElementos;i++){
+            if(this.listaTelefonos[i].obtenerTelefono() == tel){
+                this.listaTelefonos[i] = null;
+                unElementoMenos(this.listaTelefonos);
+                eliminado = true;
+            }//Fin Si
+        }//Fin Para
+        return eliminado;
+    }
+    public int buscar(String str, int pos){
+        int encontrado = -1;
+        
+        
+        while(pos < this.nElementos && encontrado == -1){
+            if(this.listaTelefonos[pos].obtenerNombre().indexOf(str) != -1){
+                encontrado = pos;
+            }//Fin Si
+            pos++;
+        }//Fin Mientras
+        return encontrado;
+        
+    }
+}
---
+++ Prog/AEF_Estructuras dinámicas/src/Ud13Act1/Persona.java
@@ -0,0 +1,46 @@
+package Ud13Act1;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andy Jan
+ */
+public class Persona {
+    
+    private String nombre;
+    private String direccion;
+    private long telefono;
+    
+    public Persona(){
+        
+    }
+    public Persona(String nom, String dir, long tel){
+        this.nombre = nom;
+        this.direccion = dir;
+        this.telefono = tel;
+    }
+    
+    public void asignarNombre(String nom){
+        this.nombre = nom;
+    }
+    public String obtenerNombre(){
+        return this.nombre;
+    }
+    public void asignarDireccion(String dir){
+        this.direccion = dir;
+    }
+    public String obtenerDireccion(){
+        return this.direccion;
+    }
+    public void asignarTelefono(long tel){
+        this.telefono = tel;
+    }
+    public long obtenerTelefono(){
+        return this.telefono;
+    }
+
+}
