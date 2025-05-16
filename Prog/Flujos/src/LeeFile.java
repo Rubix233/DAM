c8c773ad3fd2cdc85983122b017ae700a2c292a3
Rubix233 <andyjan.20@campuscamara.es>
2025-05-16 12:58:38 +0200

    Flujos

---
+++ Prog/Flujos/src/LeeFile.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andy
+ */
+
+/*
+ * Leer byte, short, int, long, float, double. Un programa para cada uno
+ */
+public class LeeFile {
+    public static void main(String[] args) throws FileNotFoundException{
+        BufferedReader file;
+        
+        file = new BufferedReader(new FileReader(""));
+        
+        
+        
+        
+        
+        
+    }
+}
