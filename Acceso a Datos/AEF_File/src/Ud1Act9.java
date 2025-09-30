3da6addde738c2fac3ba2217d29ff6a25646f4e5
Rubix233 <81692750+Rubix233@users.noreply.github.com>
2025-09-30 10:05:03 +0200

    Update Ud1Act9.java

--- Acceso a Datos/AEF_File/src/Ud1Act9.java
+++ Acceso a Datos/AEF_File/src/Ud1Act9.java
@@ -3,10 +3,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 /**
  * Realizar una aplicación Java que añada al fichero copiado anteriormente un *
  * al final.
