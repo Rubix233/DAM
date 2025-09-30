60b693d711d40a5cc2ad86068c2023d69ffbd72a
Rubix233 <81692750+Rubix233@users.noreply.github.com>
2025-09-30 10:05:13 +0200

    Update Ud1Act8.java

--- Acceso a Datos/AEF_File/src/Ud1Act8.java
+++ Acceso a Datos/AEF_File/src/Ud1Act8.java
@@ -7,10 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 /**
  *Repetir la actividad 2 pero realizando el proceso línea a línea.
