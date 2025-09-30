6878c2c16cbb33eed7678f0aa399ef1692742bd0
Rubix233 <81692750+Rubix233@users.noreply.github.com>
2025-09-30 10:04:38 +0200

    Update Ud1Act11.java

--- Acceso a Datos/AEF_File/src/Ud1Act11.java
+++ Acceso a Datos/AEF_File/src/Ud1Act11.java
@@ -5,10 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 /**
  * Realizar una aplicación Java que al escribir java more fic.txt muestre el
  * contenido de dicho fichero paginado de 10 en 10 líneas (Pulse intro para
